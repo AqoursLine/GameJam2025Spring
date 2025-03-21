b5dbf559eb4383ca8c71c9e06ca1c9dd9791c0c1
AqoursLine <148611986+AqoursLine@users.noreply.github.com>
2025-03-21 16:33:14 +0900

    テクスチャ追加

---
+++ Assets/Project/Scripts/GoalScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
+using UnityEngine;
+
+public class GoalScript : MonoBehaviour {
+	[SerializeField]
+	LayerMask goalMask;
+
+	[SerializeField]
+	ClearScript clearScript;
+
+	MovePlayer movePlayer = null;
+
+	bool isGoal = false;
+	// Start is called before the first frame update
+	void Start() {
+		movePlayer = GetComponent<MovePlayer>();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (!isGoal) {
+			RaycastHit2D hit = Physics2D.CircleCast((Vector2)transform.position, 0.3f, Vector2.up, 0.1f, goalMask);
+			if (hit.collider != null) {
+				isGoal = true;
+			}
+		}
+
+		//プレイヤーのスクリプトがある
+		if (isGoal) {
+			//プレイヤーの移動が終わったら
+			if (!movePlayer.GetPlayerState()) {
+				clearScript.IsClearEffect();
+				movePlayer.enabled = false;
+			}
+
+			isGoal = false;
+		}
+	}
+}
